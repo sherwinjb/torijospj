2052cfa0d95f893ff21d38f685820edb47579699
Serwin Torijos <128556924+sherwinjb@users.noreply.github.com>
2024-10-26 11:20:04 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const sections = document.querySelectorAll('section');
+
+window.addEventListener('scroll', () => {
+  sections.forEach(section => {
+    const sectionTop = section.getBoundingClientRect().top;
+    if (sectionTop < window.innerHeight - 100) {
+      section.classList.add('visible');
+    }
+  });
+});
